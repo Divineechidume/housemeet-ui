b000d1376f4a30457370a52ef7e2179eafae871b
Divineechidume <divineechidume@berkeley.edu>
2020-07-09 22:59:43 -0700

    successfully set up routing

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import HomePage from "./HomePage";
-import { Helmet } from "react-helmet";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import HomePage from "./HomePage";
+
+/**The App class is class component rather than a functional compnent on the off chance that I decide to maintain state. */
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -11,19 +13,24 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <header>
-          <Helmet>
-            <title>housemeet</title>
-          </Helmet>
-        </header>
-        ;<Home />
+        <Router>
+          <div>
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
 }
 
 function Home() {
-  return HomePage;
+  return <HomePage />;
 }
 
 export default App;
